e92479763f0aa7c79ff28ae46a035c56d2fab55a
quilleee <83057216+quilleee@users.noreply.github.com>
2022-03-06 12:58:45 +1100

    auto2

--- src/main/java/frc/robot/commands/Group/Autonomous/Autonomous2/A2M1.java
+++ src/main/java/frc/robot/commands/Group/Autonomous/Autonomous2/A2M1.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Group.Autonomous.Autonomous1;
+package frc.robot.commands.Group.Autonomous.Autonomous2;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -14,6 +14,8 @@ import frc.robot.commands.IntakeOut;
 import frc.robot.commands.Profiled2dMovement;
 import frc.robot.commands.ShootBall;
 import frc.robot.commands.Group.Autonomous.setRobotPosition;
+import frc.robot.commands.Group.Autonomous.Autonomous1.IntakeDrive;
+import frc.robot.commands.Group.Autonomous.Autonomous1.ShootAndFeed;
 import frc.robot.commands.Group.Group.Feeder.IndexFirstBall;
 import frc.robot.commands.Group.Group.Intake.IntakeMaster;
 import frc.robot.subsystems.Intake;
@@ -26,21 +28,18 @@ import frc.robot.subsystems.drivetrain.DrivetrainConstants;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class A1M1 extends SequentialCommandGroup {
+public class A2M1 extends SequentialCommandGroup {
 
   private final Drivetrain drivetrain = new Drivetrain();
 
 
-  public A1M1(Intake intake,Feeder feeder, Drivetrain drivetrain, Shooter shooter) {
+  public A2M1(Drivetrain drivetrain, Feeder feeder, Shooter shooter) {
   
     addCommands(
-    new IntakeDrive(drivetrain, intake, feeder),
-    //new Profiled2dMovement(drivetrain, DrivetrainConstants.movementParameters, new Pose2d(1.09, 0.92, Rotation2d.fromDegrees(21))),
-   // new IntakeMaster(intake, feeder),
-    //new IntakeDrive(drivetrain, intake, feeder).withTimeout(5),
-    new Profiled2dMovement(drivetrain, DrivetrainConstants.movementParameters, new Pose2d(0.84, 0.6, Rotation2d.fromDegrees(-167))),
-    new ShootAndFeed (shooter, feeder),
-    new Profiled2dMovement(drivetrain, DrivetrainConstants.movementParameters, new Pose2d(1.5, 0.6, Rotation2d.fromDegrees(-167)))
+ 
+    new Profiled2dMovement(drivetrain, DrivetrainConstants.movementParameters, new Pose2d(-1, 0, Rotation2d.fromDegrees(0))),
+    new ShootAndFeed2 (shooter, feeder),
+    new Profiled2dMovement(drivetrain, DrivetrainConstants.movementParameters, new Pose2d(-2, 0, Rotation2d.fromDegrees(0)))
 
 
     );
